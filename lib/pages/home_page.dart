6ade73a19200d03eb47990bc27da78fc059037c9
fshaqsty <f4risharazak@gmail.com>
2023-06-06 01:58:47 +0800

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,38 +19,33 @@ class _HomePageState extends State<HomePage> {
 
   Future<List<FerryTicket>> _getFerryTicket() async {
     return await _databaseService.ferryTicket();
-    
   }
-
-
 
   Future<void> _onFerryTicketDelete(FerryTicket ferryTicket) async {
     await _databaseService.deleteBooking(ferryTicket.book_id!);
     setState(() {});
   }
 
-
   @override
   Widget build(BuildContext context) {
-   
     return DefaultTabController(
       length: 1,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Ferry Ticket '),
           centerTitle: true,
-          bottom:  TabBar(
+          bottom: TabBar(
             tabs: [
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0),
                 child: Text(widget.userID),
               ),
-             
             ],
           ),
         ),
         body: TabBarView(
-          children: [ // This one dislay shoes
+          children: [
+            // This one dislay shoes
             FerryTicketBuilder(
               future: _getFerryTicket(),
               onEdit: (value) {
@@ -58,28 +53,31 @@ class _HomePageState extends State<HomePage> {
                   Navigator.of(context)
                       .push(
                         MaterialPageRoute(
-                          builder: (_) => BookingFormPage(ferryTicket: value, userID: widget.userID,),
+                          builder: (_) => BookingFormPage(
+                            ferryTicket: value,
+                            userID: widget.userID,
+                          ),
                           fullscreenDialog: true,
                         ),
                       )
-                      .then((_) => setState(() {})); 
+                      .then((_) => setState(() {}));
                 }
               },
-              onDelete: _onFerryTicketDelete, // Delet 
+              onDelete: _onFerryTicketDelete, // Delet
             ),
-            
           ],
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-          
             FloatingActionButton(
               onPressed: () {
                 Navigator.of(context) // Nav to shoehomepage
                     .pushReplacement(
                       MaterialPageRoute(
-                        builder: (context) => BookingFormPage(userID: widget.userID,),
+                        builder: (context) => BookingFormPage(
+                          userID: widget.userID,
+                        ),
                         fullscreenDialog: true,
                       ),
                     )
@@ -89,12 +87,12 @@ class _HomePageState extends State<HomePage> {
               child: const FaIcon(FontAwesomeIcons.ticket),
             ),
             ElevatedButton(
-        child: const Text('Logout'),
-        onPressed: () {
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) => LoginPage()));
-        },
-      )
+              child: Icon(Icons.logout_rounded),
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => LoginPage()));
+              },
+            )
           ],
         ),
       ),
