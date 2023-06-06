4d68672d81ae15c00768b1d50906b234990ed5ce
fshaqsty <f4risharazak@gmail.com>
2023-06-06 11:21:49 +0800

    Update login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,87 +6,82 @@ import 'package:ferry_ticket_application/services/database_service.dart';
 
 import 'home_page.dart';
 
-class LoginPage  extends StatefulWidget {
-  
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   @override
-
   Widget build(BuildContext context) {
     bool loginError = false;
 
     return Scaffold(
-
-
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-                  'assets/ferrylogo.png',
-          width: MediaQuery.of(context).size.width / 2,
-            ),
-
-          Container(
-              alignment: Alignment.center,
-              child: const Text(
-                'Please login to view your bookings',
-                style: TextStyle(fontSize: 20),
-              )),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Username',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-            child: TextField(
-              obscureText: true,
-              controller: passwordController,
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-          ),
-          Container(height: 10),
-          Container(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                child: const Text('Login'),
-                onPressed: handleLogin,
-              )),
-          Row(
-            children: <Widget>[
-              const Text('Does not have account?'),
-              TextButton(
-                  child: const Text(
-                    'Sign up',
-                    style: TextStyle(fontSize: 20),
+        body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/ferrylogo.png',
+                    width: MediaQuery.of(context).size.width / 2,
                   ),
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => SigninForm()),
-                    );
-                  }),
-            ],
-          )
-        ])));
+                  Container(
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Please login to view your bookings',
+                        style: TextStyle(fontSize: 20),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Username',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: TextField(
+                      obscureText: true,
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  Container(height: 10),
+                  Container(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        child: const Text('Login'),
+                        onPressed: handleLogin,
+                      )),
+                  Row(
+                    children: <Widget>[
+                      const Text('Does not have account?'),
+                      TextButton(
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SigninForm()),
+                            );
+                          }),
+                    ],
+                  )
+                ])));
   }
 
   Future<bool> handleLogin() async {
@@ -99,17 +94,31 @@ class _LoginPageState extends State<LoginPage> {
           await db.getUserID(nameController.text, passwordController.text);
       await Navigator.pushReplacement(
         context,
-       MaterialPageRoute(builder: (context) => HomePage(userID: userID)),
-
+        MaterialPageRoute(builder: (context) => HomePage(userID: userID)),
       );
 
       //TODO: Replace ViewBookingForm with the screen where you can manage bookings.
 
-      
       return true;
-
     } else {
-      print("Try again!");
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Login Error'),
+            content: Text('Invalid username or password.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+
       return false;
     }
   }
